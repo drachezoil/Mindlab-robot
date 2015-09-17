a45179ca3a62c4d0cc087a9140a694233245f29b
drachezoil <zoilo.abad@laposte.net>
2015-09-18 08:50:05 +1200

    Update Mindlab-robot-basic.ino

--- Mindlab-robot-basic.ino
+++ Mindlab-robot-basic.ino
@@ -1,1 +1,24 @@
-
+int motorPin1 = 3;
+int motorPin2 = 4;
+ 
+void setup() 
+{ 
+  pinMode(motorPin1, OUTPUT);
+  pinMode(motorPin2, OUTPUT);
+  Serial.begin(9600);
+  while (! Serial);
+  Serial.println("Speed 0 to 255");
+} 
+ 
+ 
+void loop() 
+{ 
+  if (Serial.available())
+  {
+    int speed = Serial.parseInt();
+    if (speed >= 0 && speed <= 255)
+    {
+      analogWrite(motorPin, speed);
+    }
+  }
+} 
