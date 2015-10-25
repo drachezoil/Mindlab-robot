f3217425675e75ea426c9e317c2349bc332cadb4
drachezoil <zoilo.abad@laposte.net>
2015-10-25 13:00:28 +1300

    Update Test2_ultrasound.ino

--- Test2_ultrasound/Test2_ultrasound.ino
+++ Test2_ultrasound/Test2_ultrasound.ino
@@ -13,14 +13,14 @@
 
 #define led1Pin       11  // LEDs
 #define led2Pin       12  // LEDs
-#define ECHO_PIN      9   // Arduino pin tied to echo pin on ping sensor.
-#define TRIGGER_PIN   10  // Arduino pin tied to trigger pin on ping sensor.
+#define ECHO_PIN      7   // Arduino pin tied to echo pin on ping sensor.
+#define TRIGGER_PIN   8   // Arduino pin tied to trigger pin on ping sensor.
 #define buzzerPin     3   // Buzzer
 #define remotePin     4   // Ir reciever
-#define MOTOR_A_DIR   7   // Motor A pin 1, A-IB, DIR
-#define MOTOR_A_PWM   5   // Motor A pin 2, A-IA, PWM
-#define MOTOR_B_DIR   8   // Motor B pin 1, B-IB, DIR
-#define MOTOR_B_PWM   6   // Motor B pin 2, B-IA, PWM
+#define MOTOR_A_B     9   // Motor A pin 1, A-IB, DIR
+#define MOTOR_A_A     5   // Motor A pin 2, A-IA, PWM
+#define MOTOR_B_B     10  // Motor B pin 1, B-IB, DIR
+#define MOTOR_B_A     6   // Motor B pin 2, B-IA, PWM
 
 // the actual values for "fast" and "slow" depend on the motor
 #define PWM_SLOW    100      // arbitrary slow speed PWM duty cycle
@@ -40,10 +40,10 @@ void setup()
   Serial.begin(9600);
   pinMode(led1Pin,OUTPUT);          // Led output
   pinMode(led2Pin,OUTPUT);          // Led output
-  pinMode( MOTOR_A_DIR, OUTPUT );
-  pinMode( MOTOR_A_PWM, OUTPUT );
-  pinMode( MOTOR_B_DIR, OUTPUT );
-  pinMode( MOTOR_B_PWM, OUTPUT );
+  pinMode( MOTOR_A_A, OUTPUT );
+  pinMode( MOTOR_A_B, OUTPUT );
+  pinMode( MOTOR_B_A, OUTPUT );
+  pinMode( MOTOR_B_B, OUTPUT );
   stop();
   digitalWrite(led1Pin, HIGH);    
   digitalWrite(led2Pin, HIGH);    
@@ -66,63 +66,66 @@ void loop()
   
   if (sonarDistance<10) {
     stop();
-    delay(500);
+    delay(100);
     backward(PWM_SLOW);
     delay(500);
+    stop();
+    delay(100);
     right(PWM_SLOW);
     delay(500);
-
+    stop();
+    delay(100);
   } else {
-    forward(PWM_FAST);
+    forward(PWM_SLOW);
   }
 }
 
 void forward(int speed)
 {
-  analogWrite(MOTOR_A_PWM,speed);//input a simulation value to set the speed
-  analogWrite(MOTOR_B_PWM,speed);
-  digitalWrite(MOTOR_A_DIR,HIGH);//turn DC Motor B move clockwise
-  digitalWrite(MOTOR_B_DIR,HIGH);//turn DC Motor A move clockwise
+  digitalWrite(MOTOR_A_A,LOW);
+  digitalWrite(MOTOR_B_A,LOW);
+  analogWrite(MOTOR_A_B,speed);
+  analogWrite(MOTOR_B_B,speed);
   digitalWrite(led1Pin, HIGH);    
   digitalWrite(led2Pin, HIGH);
 }
 
 void right(int speed)
 {
-  analogWrite(MOTOR_A_PWM,speed);//input a simulation value to set the speed
-  analogWrite(MOTOR_B_PWM,speed);
-  digitalWrite(MOTOR_A_DIR,HIGH);//turn DC Motor B move clockwise
-  digitalWrite(MOTOR_B_DIR,LOW);//turn DC Motor A move anticlockwise
+  analogWrite(MOTOR_A_A,speed);
+  digitalWrite(MOTOR_B_A,LOW);
+  digitalWrite(MOTOR_A_B,LOW);
+  analogWrite(MOTOR_B_B,speed);
   digitalWrite(led1Pin, HIGH);   
   digitalWrite(led2Pin, LOW);    
 }
 
 void left(int speed)
 {
-  analogWrite(MOTOR_A_PWM,speed);//input a simulation value to set the speed
-  analogWrite(MOTOR_B_PWM,speed);
-  digitalWrite(MOTOR_A_DIR,LOW);//turn DC Motor B move clockwise
-  digitalWrite(MOTOR_B_DIR,HIGH);//turn DC Motor A move anticlockwise
+  digitalWrite(MOTOR_A_A,LOW);
+  analogWrite(MOTOR_B_A,speed);
+  analogWrite(MOTOR_A_B,speed);
+  digitalWrite(MOTOR_B_B,LOW);
   digitalWrite(led2Pin, HIGH);
   digitalWrite(led1Pin, LOW); 
 }
 
 void backward(int speed)
 {
-  analogWrite(MOTOR_A_PWM,speed);//input a simulation value to set the speed
-  analogWrite(MOTOR_B_PWM,speed);
-  digitalWrite(MOTOR_A_DIR,LOW);//turn DC Motor B move anticlockwise
-  digitalWrite(MOTOR_B_DIR,LOW);//turn DC Motor A move anticlockwise
+  analogWrite(MOTOR_A_A,speed);
+  analogWrite(MOTOR_B_A,speed);
+  digitalWrite(MOTOR_A_B,LOW);
+  digitalWrite(MOTOR_B_B,LOW);
   digitalWrite(led1Pin, LOW);   
   digitalWrite(led2Pin, LOW); 
 }
 
 void stop()
 {
-  digitalWrite(MOTOR_A_PWM,LOW);// Unenble the pin, to stop the motor. this should be done to avid damaging the motor.
-  digitalWrite(MOTOR_B_PWM,LOW);
-  digitalWrite(MOTOR_A_DIR,LOW);//turn DC Motor B move clockwise
-  digitalWrite(MOTOR_B_DIR,LOW);//turn DC Motor A move clockwise
+  digitalWrite(MOTOR_A_A,LOW);
+  digitalWrite(MOTOR_B_A,LOW);
+  digitalWrite(MOTOR_A_B,LOW);
+  digitalWrite(MOTOR_B_B,LOW);
   digitalWrite(led1Pin, LOW);   
   digitalWrite(led2Pin, LOW); 
 }
